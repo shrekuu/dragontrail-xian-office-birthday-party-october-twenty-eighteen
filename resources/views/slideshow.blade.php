373e0b21d194bce64422e644820fb3b1dc375f0e
bond <shrekuu@163.com>
2018-10-31 10:43:15 +0800

    init repo

---
+++ resources/views/slideshow.blade.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="zh-cn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>龙途互动西安办公室2018年10月生日会</title>
+
+    <!-- Google Fonts, only use css, fonts are dilivered by Google China -->
+    <link href="/vendor/webslides/css/fonts.css" rel="stylesheet">
+
+    <!-- CSS Base + Colors -->
+
+    <link rel="stylesheet" type="text/css" media="all" href="/vendor/webslides/css/webslides.css">
+
+    <!-- Optional - CSS SVG Icons (Font Awesome) -->
+    <link rel="stylesheet" type="text/css" media="all" href="/vendor/webslides/css/svg-icons.css">
+</head>
+<body>
+
+<main role="main">
+<article id="webslides">
+
+
+    <section>
+        <div class="wrap aligncenter">
+            <h2><strong>龙途互动西安办公室 2018 年 10 月生日会</strong></h2>
+
+            <p class="text-muted">from 产品创新部</p>
+        </div>
+    </section>
+
+
+    <section><span class="background" style="background-image:url('/images/lamp.png');background-position:center center; background-size:cover;background-repeat: no-repeat;"></span></section>
+
+
+    <section><span class="background" style="background: transparent url('/images/keyboard.png') center center no-repeat; background-size:cover;"></span></section>
+
+
+    <section><span class="background" style="background: transparent url('/images/snacks.png') center center no-repeat; background-size: cover;"></span></section>
+
+
+    <section class="aligncenter"><!--.wrap = container (width: 90%) -->
+        <div class="wrap">
+            <div class="content-center">
+                <h3><strong>真假难辨游戏规则</strong></h3>
+
+                <ul class="flexblock specs rule-list">
+                    <li>
+                        <div>
+                            <h2>1、每个部门找4个小伙伴。</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>2、为每个队伍发放4个一次性纸杯，其中有一杯为不明可食用安全无毒害的液体（例如：白醋、白酒、加了芥末油的白开水等）。</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>3、部门间循环比拼， A -&gt; B -&gt; C -&gt; D -&gt; E -&gt; F -&gt; A。后一支队伍先喝，前一支队伍根据后一支队伍的表现猜测谁喝到了不明液体（例如：白醋、啤酒）。猜中积2分，猜不中对面队伍积1分。</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>4、每个队伍猜两次，经过一轮循环后，积分最高的队伍获胜，如有平分，则平分玩家再进行一轮比拼，决出胜利团队。</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>PS: 胜利团队有神秘大奖，希望各个部门的小哥哥小姐姐踊跃参加</h2>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- end .content-center --></div>
+        <!-- .end .wrap --></section>
+
+
+    <section><span class="background" style="background:white;"></span>
+        <div class="wrap">
+            <div class="content-center">
+                <h3><strong>寿星抽奖</strong></h3>
+            </div>
+
+            <div>&nbsp;<br>
+                &nbsp;</div>
+
+            <p style="text-align:center"><img alt="" height="430" src="/images/qr-code.jpeg" width="430"></p>
+
+            <p>&nbsp;</p>
+        </div>
+    </section>
+
+
+    <section class="aligncenter"><!--.wrap = container (width: 90%) -->
+        <div class="wrap">
+            <div class="content-center">
+                <h3><strong>飞花令游戏规则</strong></h3>
+
+                <p>一种行酒令，按抽签顺序每人轮流在规定时间内说出一句包含主题字的诗词，循环往复，对令人在规定时间未说出诗句，则淘汰，然后更换主题字，进入下一轮，纸质决出优胜者。</p>
+
+                <ul class="flexblock specs rule-list">
+                    <li>
+                        <div>
+                            <h2>1.体裁：诗（诗经、乐府、楚辞、唐诗等，不包含现代诗）、词、赋、曲、骈文</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>2.主持人行令，选手依次对令，每人有30秒思考及作答时间，从前一对令人作答结束开始计算</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>3.抽签决定答题顺序，看报名人数决定是否分组</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>4.同一组主题字（或者说同一轮）诗句不可重复，每轮会有多个主题字，诗句中只需包含任意一个主题字即可</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h2>5.主题字（组）：春江花月夜 风雪雨雾 夏秋冬（备用：青黄朱（红/朱）白 人物典故）</h2>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- end .content-center --></div>
+        <!-- .end .wrap --></section>
+
+
+    <section><span class="background" style="background:white;"></span>
+        <div class="wrap">
+            <div class="content-center">
+                <h3><strong>寿星抽奖</strong></h3>
+            </div>
+
+            <div>&nbsp;<br>
+                &nbsp;</div>
+
+            <p style="text-align:center"><img alt="" height="430" src="/images/qr-code.jpeg" width="430"></p>
+
+            <p>&nbsp;</p>
+        </div>
+    </section>
+
+
+</article>
+</main>
+<!--main-->
+
+
+<!-- Required -->
+<script src="/vendor/webslides/js/webslides.js"></script>
+
+<script>
+    window.ws = new WebSlides();
+</script>
+<!-- Autoslide? window.ws = new WebSlides({ autoslide: 8000 }); -->
+
+<!-- OPTIONAL - svg-icons.js (fontastic.me - Font Awesome as svg icons) -->
+<script defer src="/vendor/webslides/js/svg-icons.js"></script>
+
+</body>
+</html>
